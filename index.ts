7f2ba561fdd41634dcd5bac5fa5816cc59ea1c40
Simon Emms <simon@slashdevslashnull.it>
2016-02-29 19:45:15 +0000

    Configured the tests

---
+++ index.ts
@@ -0,0 +1,23 @@
+/**
+ * index
+ */
+
+"use strict";
+
+
+/* Node modules */
+
+
+/* Third-party modules */
+
+
+/* Files */
+
+
+export class Restify {
+
+
+    protected _twat: string = "fart";
+
+
+}
---
+++ test/index.test.ts
@@ -0,0 +1,27 @@
+/**
+ * index
+ */
+
+"use strict";
+
+
+/* Node modules */
+
+
+/* Third-party modules */
+import * as chai from "chai";
+import * as sinon from "sinon";
+import sinonChai = require("sinon-chai");
+
+chai.use(sinonChai);
+
+let expect = chai.expect;
+
+
+/* Files */
+
+
+describe("index test", function () {
+
+
+});
